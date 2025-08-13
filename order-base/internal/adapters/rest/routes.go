2b59f52b73f812de35e0164e7d8d2fb505982843
Util787 <georgiy.chuprina.787@mail.ru>
2025-08-14 02:13:35 +0300

    feat(order-base): added server and routes

--- order-base/internal/adapters/rest/routes.go
+++ order-base/internal/adapters/rest/routes.go
@@ -1,1 +1,28 @@
 package rest
+
+import (
+	"github.com/Util787/order-base/internal/config"
+	"github.com/gin-gonic/gin"
+)
+
+func (h *Handler) InitRoutes(env string) *gin.Engine {
+	if env == config.EnvProd {
+		gin.SetMode(gin.ReleaseMode)
+	}
+	router := gin.New()
+
+	if env != config.EnvProd {
+		router.Use(gin.Logger())
+	}
+
+	v1 := router.Group("/api/v1")
+	v1.Use(NewBasicMiddleware(h.log))
+	
+	{
+		orders := v1.Group("/orders")
+		{
+			orders.GET("/:order_id", h.getOrderById)
+		}
+	}
+	return router
+}
--- order-base/internal/adapters/rest/server.go
+++ order-base/internal/adapters/rest/server.go
@@ -1,1 +1,44 @@
 package rest
+
+import (
+	"context"
+	"log/slog"
+	"net/http"
+	"strconv"
+
+	"github.com/Util787/order-base/internal/config"
+)
+
+const maxHeaderBytes = 1 << 20 // 1 MB
+
+type Server struct {
+	httpServer *http.Server
+}
+
+func NewHTTPServer(env string, config config.HTTPServerConfig, log *slog.Logger, orderUsecase OrderUsecase) Server {
+	handler := Handler{
+		log:          log,
+		orderUsecase: orderUsecase,
+	}
+
+	httpServer := &http.Server{
+		Addr:              config.Host + ":" + strconv.Itoa(config.Port),
+		Handler:           handler.InitRoutes(env),
+		MaxHeaderBytes:    maxHeaderBytes,
+		ReadHeaderTimeout: config.ReadHeaderTimeout,
+		WriteTimeout:      config.WriteTimeout,
+		ReadTimeout:       config.ReadTimeout,
+	}
+
+	return Server{
+		httpServer: httpServer,
+	}
+}
+
+func (s *Server) Run() error {
+	return s.httpServer.ListenAndServe()
+}
+
+func (s *Server) Shutdown(ctx context.Context) error {
+	return s.httpServer.Shutdown(ctx)
+}
