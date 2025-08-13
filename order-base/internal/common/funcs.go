1ec0bc6172a668ea3f91b82d49ee0ed4b2620ca2
Util787 <georgiy.chuprina.787@mail.ru>
2025-08-13 23:12:21 +0300

    feat(order-base): added GetOperationName common func

--- order-base/internal/common/funcs.go
+++ order-base/internal/common/funcs.go
@@ -1,1 +1,16 @@
 package common
+
+import "runtime"
+
+// GetOperationName returns PackageName.FunctionName
+//
+// it should be used for logging or error wrapping
+func GetOperationName() string {
+
+	function, _, _, ok := runtime.Caller(1)
+	if !ok {
+		return "couldnt get op name"
+	}
+
+	return runtime.FuncForPC(function).Name()
+}
