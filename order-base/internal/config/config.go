cd5c1ccc33c169a500d5d81d29d7b60c9f19422f
Util787 <georgiy.chuprina.787@mail.ru>
2025-08-14 21:17:14 +0300

    feat(order-base): added config load from .env, updated env example

--- order-base/internal/config/config.go
+++ order-base/internal/config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"fmt"
 	"os"
 	"time"
 
@@ -15,58 +16,68 @@ const (
 )
 
 type Config struct {
-	Env              string        `yaml:"env"`
-	ShutdownTimeout  time.Duration `yaml:"shutdown-timeout"`
+	Env              string        `yaml:"env" env:"ENV"`
+	ShutdownTimeout  time.Duration `yaml:"shutdown-timeout" env:"SHUTDOWN_TIMEOUT"`
 	PostgresConfig   `yaml:"postgres"`
 	HTTPServerConfig `yaml:"http-server"`
 	KafkaConfig      `yaml:"kafka"`
 }
 
 type PostgresConfig struct {
-	Host     string `yaml:"host"`
-	Port     int    `yaml:"port"`
-	DbName   string `yaml:"db-name"`
-	User     string `yaml:"user"`
-	Password string `yaml:"password"`
+	Host     string `yaml:"host" env:"POSTGRES_HOST"`
+	Port     int    `yaml:"port" env:"POSTGRES_PORT"`
+	DbName   string `yaml:"db-name" env:"POSTGRES_DB_NAME"`
+	User     string `yaml:"user" env:"POSTGRES_USER"`
+	Password string `yaml:"password" env:"POSTGRES_PASSWORD"`
 
-	MaxConns        int           `yaml:"max-conns"`
-	ConnMaxLifetime time.Duration `yaml:"conn-max-lifetime"`
-	ConnMaxIdleTime time.Duration `yaml:"conn-max-idle-time"`
+	MaxConns        int           `yaml:"max-conns" env:"POSTGRES_MAX_CONNS"`
+	ConnMaxLifetime time.Duration `yaml:"conn-max-lifetime" env:"POSTGRES_CONN_MAX_LIFETIME"`
+	ConnMaxIdleTime time.Duration `yaml:"conn-max-idle-time" env:"POSTGRES_CONN_MAX_IDLE_TIME"`
 }
 
 type HTTPServerConfig struct {
-	Host              string        `yaml:"host"`
-	Port              int           `yaml:"port"`
-	ReadHeaderTimeout time.Duration `yaml:"read-header-timeout"`
-	WriteTimeout      time.Duration `yaml:"write-timeout"`
-	ReadTimeout       time.Duration `yaml:"read-timeout"`
+	Host              string        `yaml:"host" env:"HTTP_SERVER_HOST"`
+	Port              int           `yaml:"port" env:"HTTP_SERVER_PORT"`
+	ReadHeaderTimeout time.Duration `yaml:"read-header-timeout" env:"HTTP_SERVER_READ_HEADER_TIMEOUT"`
+	WriteTimeout      time.Duration `yaml:"write-timeout" env:"HTTP_SERVER_WRITE_TIMEOUT"`
+	ReadTimeout       time.Duration `yaml:"read-timeout" env:"HTTP_SERVER_READ_TIMEOUT"`
 }
 
 type KafkaConfig struct {
-	Brokers []string      `yaml:"brokers"`
-	Topic   string        `yaml:"topic"`
-	GroupID string        `yaml:"group-id"`
-	MaxWait time.Duration `yaml:"max-wait"`
+	Brokers []string      `yaml:"brokers" env:"KAFKA_BROKERS"`
+	Topic   string        `yaml:"topic" env:"KAFKA_TOPIC"`
+	GroupID string        `yaml:"group-id" env:"KAFKA_GROUP_ID"`
+	MaxWait time.Duration `yaml:"max-wait" env:"KAFKA_MAX_WAIT"`
 }
 
-// MustLoadConfig expects CONFIG_PATH environment variable to be already set
+// If CONFIG_PATH env variable is set it will load from yaml, if not it will load from env
 func MustLoadConfig() *Config {
 	err := godotenv.Load()
 	if err != nil {
-		panic("Failed to load .env file")
+		panic("Failed to load .env file: " + err.Error())
 	}
 
 	path := os.Getenv("CONFIG_PATH")
 
-	if _, err = os.Stat(path); os.IsNotExist(err) {
-		panic("Config path does not exist")
-	}
-
 	var cfg Config
 
-	err = cleanenv.ReadConfig(path, &cfg)
-	if err != nil {
-		panic("Error reading config: " + err.Error())
+	if path == "" {
+		fmt.Println("CONFIG_PATH is not set: load from .env")
+		err = cleanenv.ReadEnv(&cfg)
+		if err != nil {
+			panic("Error reading env: " + err.Error())
+		}
+	} else {
+		fmt.Println("CONFIG_PATH is set: load from .yaml")
+		configFile, err := os.Open(path)
+		if err != nil {
+			panic("Error opening config file: " + err.Error())
+		}
+		defer configFile.Close()
+		err = cleanenv.ParseYAML(configFile, &cfg)
+		if err != nil {
+			panic("Error reading config: " + err.Error())
+		}
 	}
 
 	if cfg.Env == "" {
