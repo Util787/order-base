15485c1e9f572197170eb104f3ab85d65084d922
Util787 <georgiy.chuprina.787@mail.ru>
2025-08-14 16:06:09 +0300

    feat(order-base): added order usecase

---
+++ order-base/internal/usecase/order_usecase.go
@@ -0,0 +1,42 @@
+package usecase
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+
+	"github.com/Util787/order-base/internal/common"
+	"github.com/Util787/order-base/internal/models"
+)
+
+func (u *OrderUsecase) GetOrderById(ctx context.Context, id string) (models.Order, error) {
+	op := common.GetOperationName()
+	log := common.LogOpAndReqId(ctx, op, u.log)
+
+	order, err := u.cacheStorage.GetOrder(ctx, id)
+	if err == nil {
+		log.Debug("found in cache", slog.String("order_id", id))
+		return order, nil
+	}
+	log.Debug("failed to found in cache, fetching from storage", slog.String("order_id", id), slog.String("error", err.Error()))
+
+	order, err = u.orderStorage.GetOrderById(ctx, id)
+	if err != nil {
+		return models.Order{}, fmt.Errorf("%s: %w", op, err)
+	}
+	return order, nil
+}
+
+func (u *OrderUsecase) SaveOrder(ctx context.Context, order models.Order) error {
+	op := common.GetOperationName()
+	log := common.LogOpAndReqId(ctx, op, u.log)
+
+	if err := u.orderStorage.SaveOrder(ctx, order); err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+	if err := u.cacheStorage.CacheOrder(ctx, order.OrderUID, order); err != nil {
+		log.Warn("failed to cache order", slog.String("order_id", order.OrderUID), slog.String("error", err.Error()))
+	}
+
+	return nil
+}
--- order-base/internal/usecase/usecase.go
+++ order-base/internal/usecase/usecase.go
@@ -1,1 +1,32 @@
 package usecase
+
+import (
+	"context"
+	"log/slog"
+
+	"github.com/Util787/order-base/internal/models"
+)
+
+type OrderStorage interface {
+	GetOrderById(ctx context.Context, id string) (models.Order, error)
+	SaveOrder(ctx context.Context, order models.Order) error
+}
+
+type CacheStorage interface {
+	GetOrder(ctx context.Context, key string) (models.Order, error)
+	CacheOrder(ctx context.Context, key string, order models.Order) error
+}
+
+type OrderUsecase struct {
+	log          *slog.Logger
+	orderStorage OrderStorage
+	cacheStorage CacheStorage
+}
+
+func NewOrderUsecase(log *slog.Logger, orderStorage OrderStorage, cacheStorage CacheStorage) OrderUsecase {
+	return OrderUsecase{
+		log:          log,
+		orderStorage: orderStorage,
+		cacheStorage: cacheStorage,
+	}
+}
