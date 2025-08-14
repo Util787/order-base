c6fa0940c1cd0b11aa94e88c84fa91b3f68e336c
Util787 <georgiy.chuprina.787@mail.ru>
2025-08-14 15:41:49 +0300

    feat(order-base): added in-memory storage

---
+++ order-base/internal/infra/storage/in-memory.go
@@ -0,0 +1,67 @@
+package storage
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/Util787/order-base/internal/common"
+	"github.com/Util787/order-base/internal/models"
+)
+
+type InMemoryStorage struct {
+	orders map[string]models.Order
+}
+
+func NewInMemoryStorage(ctx context.Context, startSize int) InMemoryStorage {
+	return InMemoryStorage{
+		orders: make(map[string]models.Order, startSize),
+	}
+}
+
+type OrderStorage interface {
+	GetAllOrders(ctx context.Context, limit *uint64) ([]models.Order, error)
+}
+
+// LoadOrders loads orders from the OrderStorage into InMemoryStorage using orderUID as key.
+//
+// loadLimit specifies the maximum number of orders to load.
+func (s *InMemoryStorage) LoadOrders(ctx context.Context, orderStorage OrderStorage, loadLimit *uint64) error {
+	op := common.GetOperationName()
+
+	orders, err := orderStorage.GetAllOrders(ctx, loadLimit)
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	for _, order := range orders {
+		s.CacheOrder(ctx, order.OrderUID, order)
+	}
+	return nil
+}
+
+func (s *InMemoryStorage) CacheOrder(ctx context.Context, key string, order models.Order) error {
+	op := common.GetOperationName()
+
+	select {
+	case <-ctx.Done():
+		return fmt.Errorf("%s: %w", op, ctx.Err())
+	default:
+		s.orders[key] = order
+		return nil
+	}
+}
+
+func (s *InMemoryStorage) GetOrder(ctx context.Context, key string) (models.Order, error) {
+	op := common.GetOperationName()
+
+	select {
+	case <-ctx.Done():
+		return models.Order{}, fmt.Errorf("%s: %w", op, ctx.Err())
+	default:
+		order, exists := s.orders[key]
+		if !exists {
+			return models.Order{}, fmt.Errorf("%s: %w", op, models.ErrOrdersNotFound)
+		}
+		return order, nil
+	}
+}
