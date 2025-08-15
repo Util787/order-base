6820d9638b7c2c4be02dd342860be329285cecee
Util787 <georgiy.chuprina.787@mail.ru>
2025-08-15 19:41:50 +0300

    feat(order-base): added graceful shutdown

--- order-base/cmd/main.go
+++ order-base/cmd/main.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"log/slog"
 	"os"
+	"os/signal"
+	"syscall"
 
 	kafka_subscriber "github.com/Util787/order-base/internal/adapters/kafka-subscriber"
 	"github.com/Util787/order-base/internal/adapters/rest"
@@ -11,6 +13,18 @@ import (
 	"github.com/Util787/order-base/internal/infra/storage"
 	"github.com/Util787/order-base/internal/logger/slogpretty"
 	"github.com/Util787/order-base/internal/usecase"
+)
+
+// kafka vars
+const (
+	numFetchers    = 2
+	numHandlers    = 2
+	messageChanBuf = 100
+)
+
+// in-memory storage vars
+var (
+	loadLimit uint64 = 100
 )
 
 func main() {
@@ -23,20 +37,50 @@ func main() {
 	postgreStorage := storage.MustInitPostgres(context.Background(), cfg.PostgresConfig)
 
 	inMemoryStorage := storage.NewInMemoryStorage(context.Background(), 100)
-	var loadLimit uint64 = 1
 	inMemoryStorage.LoadOrders(context.Background(), &postgreStorage, &loadLimit)
 
 	// usecases
 	orderUsecase := usecase.NewOrderUsecase(log, &postgreStorage, &inMemoryStorage)
 
 	// kafka
-	kafkaSub := kafka_subscriber.NewKafkaSubscriber(log, cfg.KafkaConfig, &orderUsecase, 100)
-	kafkaSub.Subscribe(context.Background(), 2, 2)
+	kafkaSub := kafka_subscriber.NewKafkaSubscriber(log, cfg.KafkaConfig, &orderUsecase, messageChanBuf)
 
 	// rest
 	serv := rest.NewHTTPServer(cfg.Env, cfg.HTTPServerConfig, log, &orderUsecase)
-	log.Info("Starting HTTP server", slog.String("host", cfg.HTTPServerConfig.Host), slog.Int("port", cfg.HTTPServerConfig.Port))
-	serv.Run()
+
+	// start
+	go kafkaSub.Subscribe(context.Background(), numFetchers, numHandlers)
+
+	go func() {
+		log.Info("HTTP server start", slog.String("host", cfg.HTTPServerConfig.Host), slog.Int("port", cfg.HTTPServerConfig.Port))
+		if err := serv.Run(); err != nil {
+			log.Error("HTTP server error", slog.String("error", err.Error()))
+		}
+	}()
+
+	//graceful shutdown
+	shutDownCtx, cancel := context.WithTimeout(context.Background(), cfg.ShutdownTimeout)
+	defer cancel()
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+
+	<-quit
+	log.Info("Shutting down gracefully...")
+
+	log.Info("Shutting down server")
+	if err := serv.Shutdown(shutDownCtx); err != nil {
+		log.Error("HTTP server shutdown error", slog.String("error", err.Error()))
+	}
+
+	log.Info("Shutting down kafka subscriber")
+	if err := kafkaSub.Shutdown(); err != nil {
+		log.Error("Kafka subscriber shutdown error", slog.String("error", err.Error()))
+	}
+
+	log.Info("Shutting down postgres")
+	postgreStorage.Shutdown()
+
+	log.Info("Shutdown complete")
 }
 
 func setupLogger(env string) *slog.Logger {
